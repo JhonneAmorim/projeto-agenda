5d3ba1acee847c9a383320a7b2d4c915fdd8ff2a
JhonneAmorim <jhonneamorimdev@gmail.com>
2022-07-13 19:34:48 -0300

    Listando e apagando contatos

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,4 @@
+const { async } = require('regenerator-runtime')
 const Contato = require('../models/ContatoModel')
 
 exports.index = (req, res) => {
@@ -29,31 +30,41 @@ exports.register = async (req, res) => {
 }
 
 exports.editIndex = async (req, res) => {
-  if(!req.params.id) return res.render('404')
+  if (!req.params.id) return res.render('404')
   const contato = await Contato.buscaPorId(req.params.id)
-  if(!contato)  return res.render('404')
+  if (!contato) return res.render('404')
   res.render('contato', { contato })
 }
 
 exports.edit = async (req, res) => {
   try {
-    if(!req.params.id) return res.render('404')
+    if (!req.params.id) return res.render('404')
     const contato = new Contato(req.body)
     await contato.edit(req.params.id)
-  
+
     if (contato.errors.length > 0) {
       req.flash('errors', contato.errors)
       req.session.save(() => res.redirect('/contato/index'))
       return
     }
-  
+
     req.flash('success', 'Contato editado com sucesso.')
     req.session.save(() =>
       res.redirect(`/contato/index/${contato.contato._id}`)
     )
     return
-  } catch(e) {
+  } catch (e) {
     console.log(e)
     res.render('404')
   }
+}
+
+exports.delete = async (req, res) => {
+  if (!req.params.id) return res.render('404')
+  const contato = await Contato.delete(req.params.id)
+  if (!contato) return res.render('404')
+
+  req.flash('success', 'Contato apagdo com sucesso.')
+  req.session.save(() => res.redirect('back'))
+  return
 }
